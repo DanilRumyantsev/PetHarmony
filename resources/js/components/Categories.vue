3c7c9a22bce71a8759a0e0080061429c2f0246af
yourZaNoza <yourZaNoza@yandex.ru>
2025-04-15 00:02:14 +0300

    another commit

--- resources/js/components/Categories.vue
+++ resources/js/components/Categories.vue
@@ -22,6 +22,49 @@
             variant="flat"
             ></v-btn></p>
     </div>
+
+    <v-form v-model="valid" class="textField">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :counter="10"
+            :rules="nameRules"
+            label="Вид животного"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :counter="10"
+            :rules="nameRules"
+            label="Описание (от/до возраста)"
+            required
+            ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-btn class="button"
+            v-bind="activatorProps"
+            text="Создать"
+            variant="flat"
+            ></v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <style>
@@ -44,5 +87,34 @@
     justify-content: center;
     flex-direction: column;
     align-items: center;
+    margin-top: 25px;
+}
+.textField{
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        value => {
+          if (value) return true
+
+          return 'Поле обязательно.'
+        },
+        value => {
+          if (value?.length <= 10) return true
+
+          return 'Текст должен быть менее 10 символов'
+        },
+      ],
+      
+    }),
+  }
+</script>
